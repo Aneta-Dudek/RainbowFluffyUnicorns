3322af0e794d9b63c5a36bdb25b37cd435b1aeb9
ntiany <krolkas@gmail.com>
2020-06-08 08:47:52 +0200

    Add MVC template

---
+++ Questore/Questore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Questore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

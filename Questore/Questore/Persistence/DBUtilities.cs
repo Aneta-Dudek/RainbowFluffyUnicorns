a585f597a61cec9e5a4c118f735a78342bde576d
Jakub Starczewski <starczi@users.noreply.github.com>
2020-06-12 16:46:32 +0200

    Separate database enums

---
+++ Questore/Questore/Persistence/DBUtilities.cs
@@ -0,0 +1,20 @@
+﻿namespace Questore.Persistence
+{
+    public static class DBUtilities
+    {
+        public enum StudentEnum { Id, FirstName, LastName, Email, Coolcoins, Experience, ImageUrl }
+
+        public enum ArtifactEnum { Id, Name, Description, ImageUrl, Price }
+
+        public enum ClassEnum { Id, Name, DateStarted, ImageUrl }
+
+        public enum TeamEnum { Id, Name, ImageUrl }
+
+        public enum CategoryEnum { Id, Name }
+
+        public enum TitleEnum { Id, Name, Threshold }
+
+        public enum QuestEnum { Id, Name, Description, Reward, ImageUrl }
+
+    }
+}

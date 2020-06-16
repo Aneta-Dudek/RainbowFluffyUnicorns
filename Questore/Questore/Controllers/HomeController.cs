63ed3991808584e7fa5563a5527bd7f9e5ae0978
Aneta-Dudek <57062491+Aneta-Dudek@users.noreply.github.com>
2020-06-16 05:41:09 +0200

    Move quests and artifacts to specific views and controllers

--- Questore/Questore/Controllers/HomeController.cs
+++ Questore/Questore/Controllers/HomeController.cs
@@ -1,36 +1,13 @@
-﻿using System.Linq;
-using Microsoft.AspNetCore.Mvc;
-using Questore.Persistence;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace Questore.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly IQuestDAO _questDao;
-
-        private readonly IArtifactDAO _artifactDao;
-
-        public HomeController()
-        {
-            _questDao = new QuestDAO();
-            _artifactDao = new ArtifactDAO();
-        }
 
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Quests()
-        {
-            var quests = _questDao.GetQuests().ToList();
-            return View(quests);
-        }
-
-        public IActionResult Artifacts()
-        {
-            var artifacts = _artifactDao.GetArtifacts().ToList();
-            return View(artifacts);
         }
 
     }

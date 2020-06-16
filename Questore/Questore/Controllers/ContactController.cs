bb1ebae04f9ad03f5d6d81cbb54206f49ebeae7e
Aneta-Dudek <57062491+Aneta-Dudek@users.noreply.github.com>
2020-06-16 11:32:30 +0200

    Initialize contact menu option

---
+++ Questore/Questore/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Questore.Controllers
+{
+    public class ContactController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}

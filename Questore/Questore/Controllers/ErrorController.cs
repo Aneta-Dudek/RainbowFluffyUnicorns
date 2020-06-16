a3e4879583b5acbc1abeca51746dbbec3147657b
Aneta-Dudek <57062491+Aneta-Dudek@users.noreply.github.com>
2020-06-16 03:04:39 +0200

    Add production environment error handler

---
+++ Questore/Questore/Controllers/ErrorController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Questore.Controllers
+{
+    [ApiController]
+    public class ErrorController : ControllerBase
+    {
+
+        [Route("/Home/Error")]
+        public IActionResult Error() => RedirectToAction("Index", "Home");
+    }
+}

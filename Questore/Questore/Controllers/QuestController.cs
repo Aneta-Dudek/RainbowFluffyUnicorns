57f40a3cfcd86b0e6748b0203774a655884b0a67
Jakub Starczewski <starczi@users.noreply.github.com>
2020-06-19 20:33:15 +0200

    Add ActiveStudent prop and Claim method to QuestController

--- Questore/Questore/Controllers/QuestController.cs
+++ Questore/Questore/Controllers/QuestController.cs
@@ -1,22 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
 using Questore.Persistence;
 using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using Questore.Models;
 
 namespace Questore.Controllers
 {
     public class QuestController : Controller
     {
         private readonly IQuestDAO _questDao;
+        private readonly ISession _session;
 
-        public QuestController()
+        private Student ActiveStudent => JsonConvert.DeserializeObject<Student>(_session.GetString("user"));
+
+
+        public QuestController(IServiceProvider services)
         {
             _questDao = new QuestDAO();
+            _session = services.GetRequiredService<IHttpContextAccessor>()?.HttpContext.Session;
+
         }
 
         public IActionResult Index()
         {
             var quests = _questDao.GetQuests().ToList();
             return View(quests);
+        }
+
+        public IActionResult Claim(int id)
+        {
+            _questDao.ClaimQuest(id, ActiveStudent.Id);
+            //UPDATE SESSION
+            return RedirectToAction("Index", "Profile");
         }
     }
 }

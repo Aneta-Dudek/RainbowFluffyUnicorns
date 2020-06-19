72209253ab74dd8c813b811c537195e57ff3e80f
Jakub Starczewski <starczi@users.noreply.github.com>
2020-06-19 19:46:10 +0200

    Add Buy and CheckAffordability method

--- Questore/Questore/Controllers/ArtifactController.cs
+++ Questore/Questore/Controllers/ArtifactController.cs
@@ -1,29 +1,54 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Questore.Persistence;
-using System.Linq;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
 using Questore.Models;
+using Questore.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Questore.Controllers
 {
     public class ArtifactController : Controller
     {
         private readonly IArtifactDAO _artifactDao;
+        private readonly ISession _session;
+        private Student ActiveStudent => JsonConvert.DeserializeObject<Student>(_session.GetString("user"));
 
-        public ArtifactController()
+        public ArtifactController(IServiceProvider services)
         {
             _artifactDao = new ArtifactDAO();
+            _session = services.GetRequiredService<IHttpContextAccessor>()?.HttpContext.Session;
         }
         public IActionResult Index()
         {
             var artifacts = _artifactDao.GetArtifacts().ToList();
+            CheckAffordability(artifacts);
             return View(artifacts);
+        }
+
+        private void CheckAffordability(List<Artifact> artifacts)
+        {
+            foreach (var artifact in artifacts)
+            {
+                artifact.IsAffordable = ActiveStudent.Coolcoins >= artifact.Price;
+            }
         }
 
         [HttpPost]
         public IActionResult Use(int id)
         {
             _artifactDao.UseArtifact(id);
-            return RedirectToActionPermanent("Index", "Profile");
+            return RedirectToAction("Index", "Profile");
+        }
+
+        [HttpPost]
+        public IActionResult Buy(int id)
+        {
+            _artifactDao.BuyArtifact(id, ActiveStudent.Id);
+            //UPDATE SESSION
+            return RedirectToAction("Index", "Profile");
         }
     }
 }

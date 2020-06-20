425c7b3e85cc43aab3458ae0dc06a6a52a653b64
Aneta-Dudek <57062491+Aneta-Dudek@users.noreply.github.com>
2020-06-21 01:58:42 +0200

    Add status error and exception handler

--- Questore/Questore/Controllers/ErrorController.cs
+++ Questore/Questore/Controllers/ErrorController.cs
@@ -1,15 +1,50 @@
 ﻿using Microsoft.AspNetCore.Diagnostics;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
-using System;
+using Questore.Models;
 
 namespace Questore.Controllers
 {
-    [ApiController]
-    public class ErrorController : ControllerBase
-    {
 
-        [Route("/Home/Error")]
-        public IActionResult Error() => RedirectToAction("Index", "Home");
+    public class ErrorController : Controller
+    {
+        private ILog _iLog;
+
+        public ErrorController()
+        {
+            _iLog = Logger.GetInstance;
+        }
+
+        public IActionResult Index()
+        {
+            var exceptionDetails = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+            _iLog.LogException(exceptionDetails.Path, exceptionDetails.Error.Message, exceptionDetails.Error.StackTrace);
+            ViewBag.ExceptionPath = exceptionDetails.Path;
+            ViewBag.ExceptionMessage = exceptionDetails.Error.Message;
+            ViewBag.Stacktrace = exceptionDetails.Error.StackTrace;
+
+            return View("Error");
+
+        }
+
+
+        public IActionResult Error(int statusCode)
+        {
+            var statusCodeResult = HttpContext.Features.Get<IStatusCodeReExecuteFeature>();
+
+            switch (statusCode)
+            {
+                case 404:
+                    ViewBag.ErrorMessage = "Sorry, the resource you requested could not be found.";
+                    ViewBag.Path = statusCodeResult.OriginalPath;
+                    ViewBag.QS = statusCodeResult.OriginalQueryString;
+                    break;
+            }
+
+            return View("NotFound");
+
+        }
+
+
+
     }
 }

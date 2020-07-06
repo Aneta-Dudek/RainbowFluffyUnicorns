2c07a081816a7cf8a4ec127eecbb1374d10b2966
ntiany <krolkas@gmail.com>
2020-07-06 12:40:30 +0200

    Add attributes to preserve model state after redirect

---
+++ Questore/Questore/ModelState/ExportModelStateAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Questore.ModelState
+{
+    public class ExportModelStateAttribute : ModelStateTransfer
+    {
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            //Only export when ModelState is not valid
+            if (!filterContext.ModelState.IsValid)
+            {
+                if (filterContext.Result is RedirectResult
+                    || filterContext.Result is RedirectToRouteResult
+                    || filterContext.Result is RedirectToActionResult)
+                {
+                    var controller = filterContext.Controller as Controller;
+                    if (controller != null && filterContext.ModelState != null)
+                    {
+                        var modelState = ModelStatePreserver.SerialiseModelState(filterContext.ModelState);
+                        controller.TempData[Key] = modelState;
+                    }
+                }
+            }
+
+            base.OnActionExecuted(filterContext);
+        }
+    }
+}

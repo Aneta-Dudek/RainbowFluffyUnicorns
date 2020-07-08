17f7c2218788598406e713be50cb64e7d822f118
Jakub Starczewski <starczi@users.noreply.github.com>
2020-07-08 15:43:42 +0200

    Register Artifact and Quest Services

--- Questore/Questore/ServicesExtensions/DAOExtensions.cs
+++ Questore/Questore/ServicesExtensions/DAOExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Questore.Persistence;
 using Questore.Photos;
+using Questore.Services;
 
 namespace Questore.ServicesExtensions
 {
@@ -9,7 +10,9 @@ namespace Questore.ServicesExtensions
         public static IServiceCollection AddDataProviders(this IServiceCollection services)
         {
             services.AddScoped<IArtifactDAO, ArtifactDAO>()
+                .AddScoped<IArtifactService, ArtifactService>()
                 .AddScoped<IQuestDAO, QuestDAO>()
+                .AddScoped<IQuestService, QuestService>()
                 .AddScoped<IStudentDAO, StudentDAO>()
                 .AddScoped<IDetailsDAO, DetailsDAO>()
                 .AddScoped<IPhotoAccessor, PhotoAccessor>();

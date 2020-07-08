2a0512bcae526c13f101e78d8237272ba2920af5
ntiany <krolkas@gmail.com>
2020-07-08 11:16:34 +0200

    Add authentication interface

---
+++ Questore/Questore/Data/Interfaces/IAuthentication.cs
@@ -0,0 +1,10 @@
+﻿using Questore.Data.Models;
+using Questore.Dtos;
+
+namespace Questore.Data.Interfaces
+{
+    public interface IAuthentication
+    {
+        public User Authenticate(Login login);
+    }
+}

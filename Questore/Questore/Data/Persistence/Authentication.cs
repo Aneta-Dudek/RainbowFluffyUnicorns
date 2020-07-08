5e42724fd8cff1cb6e5d796a4a96ce9c5a704ca0
ntiany <krolkas@gmail.com>
2020-07-08 11:18:18 +0200

    Change authentication to work with new db structure

--- Questore/Questore/Data/Persistence/Authentication.cs
+++ Questore/Questore/Data/Persistence/Authentication.cs
@@ -1,26 +1,57 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Npgsql;
- using Questore.Dtos;
+using Questore.Data.Dtos;
+using Questore.Data.Interfaces;
+using Questore.Data.Models;
+using Questore.Dtos;
  using Questore.Models;
 
 namespace Questore.Persistence
 {
-    public class Authentication : DefaultDAO
+    public class Authentication : DefaultDAO, IAuthentication
     {
         private readonly IStudentDAO _studentDao;
 
         private readonly string _table = "student";
+
+        private readonly string _userTb = "credential";
 
         public Authentication(IStudentDAO studentDao, IConfiguration configuration) : base(configuration)
         {
             _studentDao = studentDao;
         }
 
-        public Student Authenticate(Login login)
+        public User Authenticate(Login login)
+        {
+            var id = GetCredentialsId(login);
+            if (id == 0)
+                return null;
+
+            var query = "SELECT id " +
+                               $"FROM {_table} " +
+                               $"WHERE credentials = '{id}'";
+
+            using var connection = Connection;
+            using var commandStudent = new NpgsqlCommand(query, connection);
+            var reader = commandStudent.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                reader.Read();
+                var studentId = reader.GetInt32(0);
+                var student = _studentDao.GetStudent(studentId);
+                return student;
+            }
+
+            return null;
+        }
+
+        private int GetCredentialsId(Login login)
         {
             using var connection = Connection;
-            var query = $"SELECT id " +
-                        $"FROM {_table} " +
+            var id = 0;
+            var query = "SELECT id " +
+                        $"FROM {_userTb} " +
                         $"WHERE email = '{login.Email}' AND password = '{login.Password}';";
 
             using var command = new NpgsqlCommand(query, connection);
@@ -29,12 +60,10 @@ namespace Questore.Persistence
             if (reader.HasRows)
             {
                 reader.Read();
-                var id = reader.GetInt32(0);
-                var student = _studentDao.GetStudent(id);
-                return student;
+                id = reader.GetInt32(0);
             }
 
-            return null;
+            return id;
         }
     }
 }

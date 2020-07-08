604dcefd0851473186bf20a1a538f2b5b51ffb79
ntiany <krolkas@gmail.com>
2020-07-08 11:16:13 +0200

    Add abstract class for user dto

---
+++ Questore/Questore/Data/Dtos/UserDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Questore.Data.Dtos
+{
+    public abstract class UserDto
+    {
+        public string Firstname { get; set; }
+
+        public string Lastname { get; set; }
+
+        public string Email { get; set; }
+
+        public string Role { get; protected set; }
+    }
+}

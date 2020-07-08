75abd90ecbcbacd9f03f2929d94c23950a60578d
ntiany <krolkas@gmail.com>
2020-07-08 11:13:54 +0200

    Add admin dto

---
+++ Questore/Questore/Data/Dtos/AdminDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Questore.Data.Dtos
+{
+    public class AdminDto : UserDto
+    {
+        public AdminDto()
+        {
+            Role = "admin";
+        }
+    }
+}

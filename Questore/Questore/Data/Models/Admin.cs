072f9a3da534eaa665ffca46742f67777d6bb409
ntiany <krolkas@gmail.com>
2020-07-09 13:28:36 +0200

    Add admin model

---
+++ Questore/Questore/Data/Models/Admin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Questore.Data.Models
+{
+    public class Admin : User
+    {
+        public Admin()
+        {
+            Role = "admin";
+        }
+
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string PhotoId { get; set; }
+    }
+}

a7c60aa77154edd3470bc8a2102c93c4614e9d0d
ntiany <krolkas@gmail.com>
2020-07-09 13:28:00 +0200

    Add admin dao interface

---
+++ Questore/Questore/Data/Interfaces/IAdminDAO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Questore.Data.Models;
+
+namespace Questore.Data.Interfaces
+{
+    public interface IAdminDAO
+    {
+        public Admin GetAdmin(int id);
+    }
+}

ad1264f45d7150b9f24c5e4a899bf352bdbd47e8
Aneta-Dudek <57062491+Aneta-Dudek@users.noreply.github.com>
2020-07-16 19:36:21 +0200

    Refactor logger code

--- Questore/Questore/Data/Logger/Logger.cs
+++ Questore/Questore/Data/Logger/Logger.cs
@@ -2,26 +2,31 @@
 using System.IO;
 using System.Text;
 
-namespace Questore.Logger
+namespace Questore.Data.Logger
 {
     public class Logger : ILog
     {
         private static readonly Lazy<Logger> instance = new Lazy<Logger>();
-        public static Logger GetInstance => instance.Value;
+        public static Logger Instance => instance.Value;
 
         public void LogException(string path, string message, string stackTrace)
         {
-            string fileName = $"Exception_{DateTime.Now.ToShortDateString()}.log";
-            string filePath = $"{AppDomain.CurrentDomain.BaseDirectory}{fileName}";
-            StringBuilder sb = new StringBuilder();
-            sb.AppendLine("-----------------------------------");
-            sb.AppendLine(DateTime.Now.ToString());
-            sb.AppendLine($"Path: {path}");
-            sb.AppendLine($"Message: {message}");
-            sb.AppendLine($"Stack Trace: {stackTrace}");
+            var fileName = $"Exception_{DateTime.Now.ToShortDateString()}.log";
+            var filePath = $"{AppDomain.CurrentDomain.BaseDirectory}{fileName}";
+            var sb = new StringBuilder()
+                .AppendLine("-----------------------------------")
+                .AppendLine(DateTime.Now.ToString())
+                .AppendLine($"Path: {path}")
+                .AppendLine($"Message: {message}")
+                .AppendLine($"Stack Trace: {stackTrace}");
+
             if (!File.Exists(filePath))
+            {
                 File.Create(filePath);
+            }
+
             using StreamWriter writer = new StreamWriter(filePath, true);
+
             writer.Write(sb.ToString());
             writer.Flush();
         }

af5a3acb0e10d9b486245f44d54fdf30cbf9d445
Aneta-Dudek <57062491+Aneta-Dudek@users.noreply.github.com>
2020-07-18 13:55:08 +0200

    Add blank spaces to MockHttpSession for Krzysiek

--- Questore/XUnitTestQuestore/MockHttpSession.cs
+++ Questore/XUnitTestQuestore/MockHttpSession.cs
@@ -10,29 +10,38 @@ namespace XUnitTestQuestore
     public class MockHttpSession : ISession
     {
         readonly Dictionary<string, object> _sessionStorage = new Dictionary<string, object>();
+
         string ISession.Id => throw new NotImplementedException();
+
         bool ISession.IsAvailable => throw new NotImplementedException();
+
         IEnumerable<string> ISession.Keys => _sessionStorage.Keys;
+
         void ISession.Clear()
         {
             _sessionStorage.Clear();
         }
+
         Task ISession.CommitAsync(CancellationToken cancellationToken)
         {
             throw new NotImplementedException();
         }
+
         Task ISession.LoadAsync(CancellationToken cancellationToken)
         {
             throw new NotImplementedException();
         }
+
         void ISession.Remove(string key)
         {
             _sessionStorage.Remove(key);
         }
+
         void ISession.Set(string key, byte[] value)
         {
             _sessionStorage[key] = Encoding.UTF8.GetString(value);
         }
+
         bool ISession.TryGetValue(string key, out byte[] value)
         {
             if (_sessionStorage[key] != null)
